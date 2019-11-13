2e7b630d2be7c52358e9ad05e8b5c8d58b4d60f0
Freddie Hall <fabioyoohoo@aol.com>
2019-11-12 20:21:34 -0500

    updated

--- weather.py
+++ weather.py
@@ -1,73 +1,64 @@
 import pyowm
 from owm_token import owm_token
 from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
+import matplotlib.dates as mdates
 
+# API token
 owm = pyowm.OWM(owm_token)
-country = 'US'
 
-# Set location
-validLocation = False
-while validLocation == False:
+def legit_city(city):
     try:
-        location = 'Burlington' #input('what city or town? ')
         # forecast of 5 days out with 3 hour intervals
-        forecast = owm.three_hours_forecast(location + "," + country)
-        validLocation = True
+        owm.three_hours_forecast(city + "," + 'US')
+        return('OK, let me find that 5-day forecast!')
     except:
-        print(location, " is not a valid location.")
-        continue
+        return(city + " is not a valid location. Please try again")
 
 
-# Set days until arrival
-arrive = 100
-while arrive not in range(6):
-    arrive = 1 #int(input("How many days are there before you arrive (0-5)? "))
+# WEATHER FORECAST FUNCTION #
+def weather_forecast(location):
 
-forecastDate = datetime.now() + timedelta(days=arrive, hours=6)
+    forecast = owm.three_hours_forecast(location + "," + 'US')
+     
+    # Set days until arrival
+    arrive = 1
+    
+    forecastDate = datetime.now()
+    
+    # Get weather
+    weather_hours = []
+    for i in range (0,99,3):
+        forecastDate = datetime.now() + timedelta(days=arrive, hours=i)
+        weather_hours.append(forecast.get_weather_at(forecastDate))
+    
+    temp = []
+    detail = []
+    dt = []
+    wind = []
 
-# Get weather
-weather_hours = []
-for i in range (0,25,3):
-    forecastDate = datetime.now() + timedelta(days=arrive, hours=i)
-    weather_hours.append(forecast.get_weather_at(forecastDate))
-
-print(weather_hours)
-
-time = 0
-for weather in weather_hours:
-    description = weather.get_detailed_status()
-    clouds = weather.get_clouds()
-    temperature = weather.get_temperature()['temp'] * 9/5 - 459.67
-    wind = weather.get_wind()['speed']
-
-    print('UTC time   ', '\t', time)
-    print('description: ', '\t', description)
-    print('cloud cover: ', '\t', clouds)
-    print('temperature: ', '\t', temperature)
-    print('wind speed: ', '\t', wind)
-    print('\n')
-    time = time + 3
-try:
-    rain = weather.get_rain()['all']
-except KeyError:
-    rain = 0
-
-# OTHER MEASUREMENTS
-# weather.get_dewpoint
-# weather.get_heat_index
-# weather.get_humidex
-# weather.get_humidity
-# weather.get_pressure
-# weather.get_status
-# weather.get_snow
-# weather.get_sunrise_time
-# weather.get_sunset_time
-# weather.get_visibility_distance
-
-
-print('description: ', '\t', description)
-print('cloud cover: ', '\t', clouds)
-print('temperature: ', '\t', temperature)
-print('wind: ', '\t', wind)
-print('rain: ', '\t', rain)
-
+    for weather in weather_hours:
+        description = weather.get_detailed_status()
+        time = weather.get_reference_time()
+        temperature = weather.get_temperature()['temp'] * 9/5 - 459.67
+        wind_speed = weather.get_wind()['speed']
+        t = datetime.fromtimestamp(float(time))
+        # add to lists
+        dt.append(t)
+        temp.append(temperature)
+        detail.append(description)
+        wind.append(wind_speed)
+        
+    # Create the plot
+    fig, ax = plt.subplots(figsize=(9,5))
+    ax.plot(dt,temp,'-p',color='dodgerblue',alpha=.9, linewidth=6)
+    fig.suptitle("{}'s 5-day forecast".format(location), fontsize=22)
+    plt.ylabel('Temperature (F)', fontsize=14)
+    
+    # Affordance for Freezing 
+    plt.axhline(32, linewidth=2, color='k', linestyle='dashed')
+    # format x axis
+    ax.xaxis.set_major_formatter(DateFormatter("%a %H:%M"))
+    # save
+    plt.savefig("plots/forecast.png")
